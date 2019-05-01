f9bbdf7ce1db0bcc731cd6bea45b30483b786120
Jesse Sheehan <jesse@sheehan.nz>
2019-05-02 08:13:04 +1200

    Doc'd timer

--- src/timer.py
+++ src/timer.py
@@ -19,6 +19,9 @@ import time
 class Timer:
 
     def __init__(self, period, callback):
+        """
+            Creates a new Timer with a period and a callback.
+        """
         self.__period = period
         self.__callback = callback
         self.__started = False
@@ -28,6 +31,9 @@ class Timer:
         self.__updateTime = 0
 
     def start(self):
+        """
+            Starts the timer.
+        """
         if not self.__started:
             t = time.time()
             self.__started = True
@@ -37,6 +43,9 @@ class Timer:
             self.__updateTime = t
 
     def stop(self):
+        """
+            Stops the timer.
+        """
         if self.__started:
             self.__started = False
             self.__startedTime = 0
@@ -45,23 +54,35 @@ class Timer:
             self.__updateTime = 0
 
     def reset(self):
+        """
+            Resets the timer.
+        """
         if self.__started:
             self.stop()
             self.start()
 
     def pause(self):
+        """
+            Pauses the timer.
+        """
         if self.__started and not self.__paused:
             self.__paused = True
             self.__pausedTime = time.time() - self.__startedTime
             self.__startedTime = 0
 
     def resume(self):
+        """
+            Resumes the timer.
+        """
         if self.__started and self.__paused:
             self.__startedTime = time.time() - self.__pausedTime
             self.__paused = False
             self.__pausedTime = 0
 
     def update(self):
+        """
+            Updates the timer. May call its callback.
+        """
         if self.__started and not self.__paused:
             t = time.time()
             dt = t - self.__updateTime
@@ -70,6 +91,9 @@ class Timer:
                 self.__callback(dt)
     
     def trigger(self):
+        """
+            Forcefully call the callback.
+        """
         if self.__started and not self.__paused:
             t = time.time()
             dt = t - self.__updateTime
@@ -77,6 +101,9 @@ class Timer:
             self.__callback(dt)
 
     def getElapsed(self):
+        """
+            Returns the time elapsed in seconds.
+        """
         if self.__started:
             if self.__paused:
                 return self.__pausedTime
@@ -86,6 +113,8 @@ class Timer:
 
     def isStarted(self):
         """
+            Returns True if the timer has been started.
+        
         >>> t = Timer(10, None)
         >>> t.isStarted()
         False
@@ -100,6 +129,8 @@ class Timer:
 
     def isPaused(self):
         """
+            Returns True if the timer has been paused.
+        
         >>> t = Timer(10, None)
         >>> t.isPaused()
         False
@@ -126,12 +157,18 @@ class Timer:
         return self.__paused and self.__started
 
     def __str__(self):
+        """
+            Returns a string representation of the timer.
+        """
         return "Timer <period={0:.3}s, started={1}, paused={2}, elapsed={3:.3}s>".format(self.__period, self.__started, self.__paused, self.getElapsed())
 
     def __repr__(self):
+        """
+            Returns a string representation of the timer.
+        """
         return self.__str__()
 
-
+# run doctests
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
