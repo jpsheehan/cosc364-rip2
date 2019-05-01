11726a7df02f772bea73c88800449cd617075d66
Jesse Sheehan <jps111@uclive.ac.nz>
2019-05-01 15:51:53 +1200

    Renamed __init__ to __main__

---
+++ src/__main__.py
@@ -0,0 +1,55 @@
+import sys
+import os.path
+
+import server
+import config
+
+
+def print_usage():
+    print("TODO: usage")
+
+
+def print_filename_error(filename):
+    print("TODO: filename error")
+
+
+def print_config_error(err):
+    print("TODO: config error", err)
+
+
+def main():
+
+    if len(sys.argv) != 2:
+        print_usage()
+        return -1
+
+    filename = sys.argv[1]
+    file = None
+    conf = None
+
+    if filename == '--':
+        file = sys.stdin
+    else:
+        if not os.path.exists(filename):
+            print_filename_error(filename)
+            return -1
+        else:
+            file = open(filename, "r")
+
+    try:
+        print("Reading configuration file... ", end='')
+        conf = config.Config()
+        conf.parse_file(file)
+        print("done!")
+
+        print("Starting RIP router #" + str(conf.router_id))
+        s = server.Server(conf)
+        s.start()
+
+    except Exception as err:
+        print_config_error(err)
+        return -1
+
+
+if __name__ == "__main__":
+    main()
