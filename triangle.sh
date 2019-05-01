81c62c797df3b3a942c0ddd59e9d1f63329378c3
will <will@wgc22@uclive.ac.nz>
2019-05-01 19:24:14 +1200

    linux sh

---
+++ triangle.sh
@@ -0,0 +1,3 @@
+xfce4-terminal --window -x python3 src ./configs/networks/triangle/1.conf &
+xfce4-terminal --window -x python3 src ./configs/networks/triangle/2.conf &
+xfce4-terminal --window -x python3 src ./configs/networks/triangle/3.conf
