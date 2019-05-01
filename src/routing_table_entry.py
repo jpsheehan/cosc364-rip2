2715a5d63e3c770465844613c16159ca3f6461aa
Jesse Sheehan <jesse@sheehan.nz>
2019-05-01 22:22:29 +1200

    Added pretty printing for age

--- src/routing_table_entry.py
+++ src/routing_table_entry.py
@@ -8,7 +8,7 @@ class RoutingTableEntry:
         self.garbage = False
 
     def __str__(self):
-        return "RouteTableEntry <destination={0}, nextHop={1}, cost={2}, age={3}, garbage={4}>".format(self.destination, self.nextHop, self.cost, self.age, self.garbage)
+        return "RouteTableEntry <destination={0}, nextHop={1}, cost={2}, age={3}, garbage={4}>".format(self.destination, self.nextHop, self.cost, round(self.age, 2), self.garbage)
 
     def __repr__(self):
         return self.__str__()
