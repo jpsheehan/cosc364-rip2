451c68925678ced93d7a242266d3d93d3b28a26a
Jesse Sheehan <jps111@uclive.ac.nz>
2019-05-01 12:49:42 +1200

    Added pretty printing. This will need updating when we have a proper routing entry class

--- src/routing_table.py
+++ src/routing_table.py
@@ -1,10 +1,37 @@
+import os
+
+
 class RoutingTable:
 
     def __init__(self):
-        pass
+        self.__routes = []
 
-    def add_entry(self):
-        pass
+    def add_entry(self, route):
+        self.__routes.append(route)
 
     def invalidate(self):
         pass
+
+    def __str__(self):
+        s = [
+            "+------------+------------+------------+------------+------------+",
+            "| Dest.      | Next Hop   | Total Cost | Age        | Garbage?   |",
+            "+------------+------------+------------+------------+------------+"
+        ]
+        for route in self.__routes:
+            s.append("| {0:<10} | {1:<10} | {2:<10} | {3:<10} | {4:<10} |".format(
+                route["router_id"], route["next_hop"], route["cost"], route["age"], route["garbage"]))
+        s.append("+------------+------------+------------+------------+------------+")
+        return os.linesep.join(s)
+
+
+if __name__ == "__main__":
+    r = RoutingTable()
+    r.add_entry({
+        "router_id": "A",
+        "next_hop": "B",
+        "cost": 7,
+        "age": 30,
+        "garbage": False
+    })
+    print(r)
