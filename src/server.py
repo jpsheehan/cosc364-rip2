00d4fa1882259dd99cae1ec0f73607bb47db2710
William George Cowper <wgc22@cs18240kq.canterbury.ac.nz>
2019-05-02 08:20:07 +1200

    Possibly breaking change. Only start garbage collection from a triggered update if we are using that route

--- src/server.py
+++ src/server.py
@@ -160,9 +160,10 @@ class Server:
             elif is_destination_in_table:
 
                 is_destination_garbage = destination_entry.garbage
-
+                # Flood triggered update. If we are using that route then also begin garbage collection. 
                 if packet.triggered and not is_destination_garbage:
-                    self.rt.set_garbage(route_destination, True)
+                    if route_next_hop == destination_entry.nextHop:
+                        self.rt.set_garbage(route_destination, True)
                     triggered_updates.append(destination_entry)
                     self.log("marked " + str(route_destination) + " as garbage")
 
