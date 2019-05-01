bd190e0fd9b47350d1dac165019bd966feb279f9
Jesse Sheehan <jesse@sheehan.nz>
2019-05-02 05:45:42 +1200

    Added figure 1 network configs

---
+++ figure1.sh
@@ -0,0 +1,7 @@
+gnome-terminal --title "Router 1" --window -e "bash -c \"python3 src ./configs/networks/figure1/1.conf; exec bash\"" &
+gnome-terminal --title "Router 2" --window -e "bash -c \"python3 src ./configs/networks/figure1/2.conf; exec bash\"" &
+gnome-terminal --title "Router 3" --window -e "bash -c \"python3 src ./configs/networks/figure1/3.conf; exec bash\"" &
+gnome-terminal --title "Router 4" --window -e "bash -c \"python3 src ./configs/networks/figure1/4.conf; exec bash\"" &
+gnome-terminal --title "Router 5" --window -e "bash -c \"python3 src ./configs/networks/figure1/5.conf; exec bash\"" &
+gnome-terminal --title "Router 6" --window -e "bash -c \"python3 src ./configs/networks/figure1/6.conf; exec bash\"" &
+gnome-terminal --title "Router 7" --window -e "bash -c \"python3 src ./configs/networks/figure1/7.conf; exec bash\""
