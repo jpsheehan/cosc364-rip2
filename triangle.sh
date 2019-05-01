b8bc1d4a31554f1cdcb1e1d9df0546c683dc446e
Jesse Sheehan <jesse@sheehan.nz>
2019-05-01 21:45:51 +1200

    Fixed issue with traingle.sh

--- triangle.sh
+++ triangle.sh
@@ -1,3 +1,3 @@
-xfce4-terminal --window -x python3 src ./configs/networks/triangle/1.conf &
-xfce4-terminal --window -x python3 src ./configs/networks/triangle/2.conf &
-xfce4-terminal --window -x python3 src ./configs/networks/triangle/3.conf
+gnome-terminal --title "Router 1" --window -e "bash -c \"python3 src ./configs/networks/triangle/1.conf; exec bash\"" &
+gnome-terminal --title "Router 2" --window -e "bash -c \"python3 src ./configs/networks/triangle/2.conf; exec bash\"" &
+gnome-terminal --title "Router 3" --window -e "bash -c \"python3 src ./configs/networks/triangle/3.conf; exec bash\""
