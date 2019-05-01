d760e68bae4aa8afb7ec5d462af74ef78189f98e
Jesse Sheehan <jps111@uclive.ac.nz>
2019-05-01 16:37:48 +1200

    New and improved clear_terminal function

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,19 @@ import os
 
 
 def clear_terminal():
+    """
+        Clears the terminal based on the type of operating system.
+    """
+
+    # the terminal clear command for linux
     if os.name == "posix":
         os.system("clear")
+
+    # the console cls command for windows
     elif os.name == "nt":
         os.system("cls")
+
+    # otherwise, just print 25 newlines
+    else:
+        for _ in range(25):
+            print("")
